401a1f15e5c6c2c95b26ba0fd1ecf3762371c6f7
mohan-aluru <mohanaluru003@gmail.com>
2026-07-27 09:47:18 +0530

    Time: 37 ms (47.77%), Space: 83.8 MB (51.28%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.java
@@ -1,30 +1,25 @@
 class Solution {
     public int maxIceCream(int[] costs, int coins) {
-        Arrays.sort(costs);
-
-        int low = 0;
-        int high = costs.length;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (canBuy(costs, coins, mid)) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+      Arrays.sort(costs);
+      int low=0;
+      int high=costs.length;
+     
+      while(low<=high){
+        int mid=low+(high-low)/2;
+        if(find(costs,coins,mid)){
+           low=mid+1;
+        }else{
+            high=mid-1;
         }
-
-        return high;
+      }
+      return high;
     }
-
-    private boolean canBuy(int[] costs, int coins, int k) {
-        long sum = 0;
-
-        for (int i = 0; i < k; i++) {
-            sum += costs[i];
-        }
-
-        return sum <= coins;
+private static boolean find(int[] costs,int coins,int max){
+    long sum=0;
+    for(int i=0;i<max;i++){
+        sum+=costs[i];
+      
     }
+    return sum<=coins;
+}
 }
