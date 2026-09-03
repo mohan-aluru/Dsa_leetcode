36fad6f3c9029571dcb9191ee4ca3c9069148cf1
mohan-aluru <mohanaluru003@gmail.com>
2026-09-03 12:09:03 +0530

    Time: 11 ms (36.75%), Space: 59.1 MB (94.38%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -10,19 +10,44 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-      List<Integer> li=new ArrayList<>();
-      ListNode temp=head;
-      while(temp!=null){
-        li.add(temp.val);
-        temp=temp.next;
-      }
-      Collections.sort(li);
-      ListNode dummy=new ListNode(-1);
-      ListNode curr=dummy;
-      for(int i:li){
-        curr.next=new ListNode(i);
-        curr=curr.next;
-      }
-      return dummy.next;
+        if(head==null || head.next==null){
+            return head;
+        }
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        ListNode second=slow.next;
+        slow.next=null;//breaking the link is mandatory since this is linked list
+        ListNode left=sortList(head);
+        ListNode right=sortList(second);//Represented as new list
+        return merge(left,right);
+    }
+    private ListNode merge(ListNode l1,ListNode l2){
+        ListNode dummy=new ListNode(-1);
+        ListNode curr=dummy;
+        while(l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                curr.next=l1;
+                l1=l1.next;
+            }else{
+                curr.next=l2;
+                l2=l2.next;
+            }
+            curr=curr.next;
+        }
+        while(l1!=null){
+            curr.next=l1;
+            curr=curr.next;
+            l1=l1.next;
+        }
+         while(l2!=null){
+            curr.next=l2;
+            curr=curr.next;
+            l2=l2.next;
+        }
+        return dummy.next;
     }
 }
